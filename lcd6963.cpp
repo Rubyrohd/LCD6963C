89e9c010a3911c80a80c6e204514ecfaebec9dc5
Lars Pietrowski (Macbook) <development@brutaldesign.dk>
2018-03-06 17:21:49 +0100

    initial files

---
+++ lcd6963.cpp
@@ -0,0 +1,12 @@
+#include "Arduino.h"
+#include "LCD6963C.h"
+
+LCD6963C::LCD6963C()
+{
+	//constructor
+}
+
+void LCD6963C::init() 
+{
+	//display initialization
+}
---
+++ lcd6963.h
@@ -0,0 +1,13 @@
+#ifndef LCD6963C_h
+#define LCD6963C_h
+
+#include "Arduino.h"
+
+class LCD6963C
+{
+  public:
+    LCD6963C();
+    void init();
+};
+
+#endif
